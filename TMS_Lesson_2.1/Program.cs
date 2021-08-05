7c56471cd6eb9449b8c9e0dd3a3e824018b50be4
Gredja <gredja.a@gmail.com>
2021-08-05 19:02:52 +0300

    Добавьте файлы проекта.

---
+++ TMS_Lesson_2.1/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+
+namespace TMS_Lesson_2._1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                int FiveWorksDays = 5;
+                int PozitionOnWeek;
+
+
+
+                Console.WriteLine("Укажите день недели в сокращенной форме: ");
+                string Day;
+                Day = Console.ReadLine();
+
+
+                switch (Day)
+                {
+                    case "Mon":
+                        {
+
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.BackgroundColor = ConsoleColor.White;
+
+
+                            Console.WriteLine("Monday = Понедельник");
+                            PozitionOnWeek = 1;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays));
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Monday")
+                            {
+                                Console.WriteLine("Сегодня Понедельник!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Tues":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.BackgroundColor = ConsoleColor.Black;
+
+                            Console.WriteLine("Tuesday = Вторник");
+                            PozitionOnWeek = 2;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 1));
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Tuesday")
+                            {
+                                Console.WriteLine("Сегодня Вторник!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Wed":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+
+                            Console.WriteLine("Wednesday = Среда");
+                            PozitionOnWeek = 3;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 1));
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Wednesday")
+                            {
+                                Console.WriteLine("Сегодня Среда!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Thur":
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.BackgroundColor = ConsoleColor.Blue;
+
+                            Console.WriteLine("Thursday = четверг");
+                            PozitionOnWeek = 4;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 1));
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Thursday")
+                            {
+                                Console.WriteLine("Сегодня Четверг!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Fri":
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            Console.BackgroundColor = ConsoleColor.Magenta;
+
+                            Console.WriteLine("Friday = Пятница");
+                            PozitionOnWeek = 5;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 1));
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Friday")
+                            {
+                                Console.WriteLine("Сегодня Пятница!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Sat":
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+                            Console.WriteLine("Saturday = Суббота");
+                            PozitionOnWeek = 6;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 1) + " - Сегодня выходной");
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Saturday")
+                            {
+                                Console.WriteLine("Сегодня Суббота!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    case "Sun":
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+                            Console.WriteLine("Sunday = Воскресенье");
+                            PozitionOnWeek = 7;
+                            Console.WriteLine("Порядковый номер: " + PozitionOnWeek);
+                            Console.WriteLine("Дней до выходного: " + (FiveWorksDays - PozitionOnWeek + 2) + " - Сегодня выходной");
+
+                            if (DateTime.Now.DayOfWeek.ToString() == "Sunday")
+                            {
+                                Console.WriteLine("Сегодня Воскресенье!");
+                            }
+
+                            Console.WriteLine("Нажмите, чтобы начать сначала...");
+                        }
+                        break;
+
+                    default:
+                        {
+                            Console.WriteLine("Такого дня не существует или данные введены некорректно!");
+                        }
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TMS_Lesson_2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace TMS_Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            while (true)
+            {
+                Console.Clear();
+                double FirstValue, SecondValue, result;
+                string typeOperation, lineForValue;
+
+                Console.WriteLine("Введите число: ");
+                lineForValue = Console.ReadLine();
+                var lineVal = double.TryParse(lineForValue, out FirstValue);
+                if (lineVal == true)
+                {
+
+
+
+                    Console.WriteLine("Операция над числами: ");
+                    typeOperation = Console.ReadLine();
+
+
+
+                    switch (typeOperation)
+                    {
+                        case "+":
+                            Console.WriteLine("Введите второе число: ");
+                            lineForValue = Console.ReadLine();
+                            var lineValue = double.TryParse(lineForValue, out SecondValue);
+
+                            if (lineValue == true)
+                            {
+                                result = FirstValue + SecondValue;
+                                Console.WriteLine("Сумма: " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения введены некорректно!");
+                            }
+                            break;
+
+
+                        case "-":
+                            Console.WriteLine("Введите второе число: ");
+                            lineForValue = Console.ReadLine();
+                            var lineValue2 = double.TryParse(lineForValue, out SecondValue);
+
+                            if (lineValue2 == true)
+                            {
+                                result = FirstValue - SecondValue;
+                                Console.WriteLine("Вычитание: " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения введены некорректно!");
+                            }
+                            break;
+
+
+                        case "/":
+                            Console.WriteLine("Введите второе число: ");
+                            lineForValue = Console.ReadLine();
+                            var lineValue3 = double.TryParse(lineForValue, out SecondValue);
+
+                            if (lineValue3 == true)
+                            {
+                                result = FirstValue / SecondValue;
+                                Console.WriteLine("Деление: " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения введены некорректно!");
+                            }
+                            break;
+
+
+                        case "*":
+                            Console.WriteLine("Введите второе число: ");
+                            lineForValue = Console.ReadLine();
+                            var lineValue4 = double.TryParse(lineForValue, out SecondValue);
+
+                            if (lineValue4 == true)
+                            {
+                                result = FirstValue * SecondValue;
+                                Console.WriteLine("Умножение: " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения введены некорректно!");
+                            }
+                            break;
+
+
+                        case "%":
+                            Console.WriteLine("Введите второе число: ");
+                            lineForValue = Console.ReadLine();
+                            var lineValue5 = double.TryParse(lineForValue, out SecondValue);
+
+                            if (lineValue5 == true)
+                            {
+                                result = FirstValue * SecondValue / 100;
+                                Console.WriteLine("Процент от числа: " + result);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения введены некорректно!");
+                            }
+                            break;
+
+
+                        case "//":
+                            result = Math.Sqrt(FirstValue);
+                            Console.WriteLine("Квадратный корень: " + result);
+                            break;
+
+                        default:
+                            Console.WriteLine("Невозможный оператор!");
+                            break;
+
+
+                    }
+                    Console.WriteLine("Желаете продолжить? - Нажмите Enter!");
+                }
+                else
+                {
+                    Console.WriteLine("Значения введены некорректно!");
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
